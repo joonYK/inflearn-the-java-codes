a8aa24d96c826a5c62fd18979d25569793487e1c
JY <kims2606@gmail.com>
2022-05-29 23:43:32 +0900

    bytecode 변경시 기존 코드를 건드리지 않고 agent 사용해서 변경

---
+++ bytecode-changing/src/main/java/jy/study/java/bytebuddy/usingAgent/Masulsa.java
@@ -0,0 +1,10 @@
+package jy.study.java.bytebuddy.usingAgent;
+
+import jy.study.java.bytebuddy.Moja;
+
+public class Masulsa {
+
+    public static void main(String[] args) {
+        System.out.println(new Moja().pullOut());
+    }
+}
